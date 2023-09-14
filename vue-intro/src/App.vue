96cb5c2c7ede7cb7fd2b0ad6cfc925488bdcf063
Ali <alidrl26@gmail.com>
2023-09-14 22:04:03 +0200

    vue intro

---
+++ vue-intro/src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>{{ greet }} {{ name }}</div>
+  <div v-text="channel"></div>
+  <div v-html="channel"></div>
+  <h2 v-bind:id="headingId">Heading</h2>
+  <button v-bind:disabled="isDisabled">Bind</button>
+</template>
+<script>
+export default {
+  name: "App",
+  data() {
+    return {
+      name: "Durul",
+      greet: "Hello",
+      channel:"<b>Codecolution</b>",
+      headingId:'heading',
+      isDisabled:false
+    };
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
