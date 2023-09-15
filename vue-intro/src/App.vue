269855a0367a9d3acf426577bc8288216dbc45b4
Ali <alidrl26@gmail.com>
2023-09-15 10:52:53 +0200

    list rendering

--- vue-intro/src/App.vue
+++ vue-intro/src/App.vue
@@ -1,16 +1,11 @@
 <template>
-  <h2 v-if="num === 1">the number zero</h2>
-  <h2 v-else-if="num < 0">the number is negative</h2>
-  <h2 v-else>the number is not zero</h2>
+  <h2 v-for="(name, index) in names" :key="name">{{ index }} {{ name }}</h2>
 
-  <div v-if="display">
-    <h2>Ali</h2>
-    <h2> "8" </h2>
-    <h2>maral</h2>
-  </div>
-
-  <h2 v-show="showElement">using v-show</h2>
-  <h2 v-if="showElement">using v-if</h2>
+  <h2 v-for="(fullname, index) in fullNames" :key="index">
+    {{ fullname.first }}
+    {{ fullname.surname }}
+    {{ fullname.age }}
+  </h2>
 </template>
 
 <script>
@@ -18,9 +13,12 @@ export default {
   name: "App",
   data() {
     return {
-      num: -5,
-      display: false,
-      showElement:false
+      names: ["ali", "maral", "lila"],
+      fullNames: [
+        { first: "ali", surname: "durul", age: 25 },
+        { first: "maral", surname: "durul", age: 26 },
+        { first: "lila", surname: "durul", age: -10 },
+      ],
     };
   },
 };
