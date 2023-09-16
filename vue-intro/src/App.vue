1c112f0f391549d5aa404440133d6f59c756f081
Ali <alidrl26@gmail.com>
2023-09-16 11:08:25 +0200

    cmputed properties

--- vue-intro/src/App.vue
+++ vue-intro/src/App.vue
@@ -1,8 +1,16 @@
 <template>
   <h2>{{ fullName }}</h2>
-  <button @click="items.push({id:4, title:'keyboard', price:55})">Add item</button>
-  <h2>
-    Totoal = {{ totalPrice }}
+  <button @click="changeFullName">change full name</button>
+  <button @click="items.push({ id: 4, title: 'keyboard', price: 55 })">
+    Add item
+  </button>
+  <h2>Total = {{ totalPrice }}</h2>
+  <h2>method Total = {{ getTotal() }}</h2>
+  <template v-for="item in items" :key="item.id">
+    <h2 v-if="item.price > 100">{{ item.title }} {{ item.price }}</h2>
+  </template>
+  <h2 v-for="item in expensiveItems" :key="item.id">
+    {{ item.title }} is {{ item.price }}
   </h2>
 </template>
 
@@ -27,19 +35,42 @@ export default {
         {
           id: 3,
           title: "Laptop",
-          price: 600,
+          price: 300,
         },
       ],
     };
   },
-  methods: {},
-  computed: {
-    fullName() {
-      return `${this.name} ${this.surName}`;
+  methods: {
+    getTotal() {
+      return this.items.reduce(
+        (total, curr) => (total = total + curr.price),
+        0
+      );
     },
-    totalPrice(){
-      return this.items.reduce((total, curr) => (total = total +curr.price), 0)
+    changeFullName(){
+      this.fullName = 'ali durul'
     }
+  },
+  computed: {
+    fullName: {
+      get() {
+        return `${this.name} ${this.surName}`;
+      },
+      set(value){
+        const names = value.split(" ")
+        this.name = names[0]
+        this.surName = names[1]
+      }
+    },
+    totalPrice() {
+      return this.items.reduce(
+        (total, curr) => (total = total + curr.price),
+        0
+      );
+    },
+    expensiveItems() {
+      return this.items.filter((item) => item.price > 100);
+    },
   },
 };
 </script>
