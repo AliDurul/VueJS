45c488b56c233ad6368c4ee5b1f3ae9732322eab
Ali <alidrl26@gmail.com>
2023-09-17 14:15:03 +0200

    deep property in watchers

--- vue-intro/src/App.vue
+++ vue-intro/src/App.vue
@@ -5,6 +5,13 @@
     <button @click="volume += 2">increment</button>
     <button @click="volume -= 2">decrease</button>
   </div>
+  <input type="text" v-model="movie" />
+  <input type="text" v-model="movieInfo.title" />
+  <input type="text" v-model="movieInfo.actor" />
+
+  <div>
+    <button @click="movieList.push('Wonder Women')">Add Movie</button>
+  </div>
 </template>
 
 <script>
@@ -13,22 +20,44 @@ export default {
   data() {
     return {
       volume: 0,
-     
+      movie: "batman",
+      movieInfo: {
+        title: "",
+        actor: "",
+      },
+      movieList: ["batman", "Superman"],
     };
   },
-  methods: {
-
-  },
-  computed: {
-   
-  },
-  watch:{
-    volume(newValue, oldValue){
-      if(newValue > oldValue && newValue===16){
-        alert('it is too much')
+  methods: {},
+  computed: {},
+  watch: {
+    volume(newValue, oldValue) {
+      if (newValue > oldValue && newValue === 16) {
+        alert("it is too much");
       }
-    }
-  }
+    },
+    movie: {
+      handler(newValue) {
+        console.log(`calling api woth movie name = ${newValue}`);
+      },
+      immediate: true,
+    },
+    movieInfo: {
+      handler(newValue) {
+        console.log(
+          `calling api with movie title = ${newValue.title} and actor = ${newValue.actor}`
+        );
+      },
+      deep: true,
+    },
+
+    movieList: {
+      handler(newValue) {
+        console.log(`updated list ${newValue}`);
+      },
+      deep: true,
+    },
+  },
 };
 </script>
 
