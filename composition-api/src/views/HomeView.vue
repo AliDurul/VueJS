5aef709e4dfc58aa343bd7cd0605cfed8f47e8ee
Ali <alidrl26@gmail.com>
2023-10-06 15:43:53 +0200

    composition computed

--- composition-api/src/views/HomeView.vue
+++ composition-api/src/views/HomeView.vue
@@ -1,14 +1,29 @@
 <template>
   <div class="home">
+<input type="text" v-model="search">
+   <div v-for="(day,i) in finded" :key="i">
+    {{ day }}
+   </div>
 
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
+import { ref, computed } from "vue";
 
 export default {
-  name: 'HomeView',
- 
-}
+  name: "HomeView",
+  components: {},
+  setup() {
+  const days = ref(['pzrtesi','sali','crmbs','prsmbe','cuma'])
+
+ const search = ref('')
+
+ const finded = computed(() => {
+   return days.value.filter(day => day.includes(search.value))
+ })
+
+    return { days,search,finded };
+  },
+};
 </script>
